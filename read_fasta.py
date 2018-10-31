6f657a80addc9eeb0e59932d167233e93c2b62c8
Tianyi Chen <tianyi.chen@duke.edu>
2018-10-31 09:51:58 -0400

    Backs out the argument number checking

--- read_fasta.py
+++ read_fasta.py
@@ -10,9 +10,5 @@ def read_fasta(filename):
     f.close()
     return sequence
 
-if len(sys.argv) < 2:
-	print('Usage: ', sys.argv[0], '<sequence.fa>')
-	exit(1)
-
 print(read_fasta(sys.argv[1]))
 
